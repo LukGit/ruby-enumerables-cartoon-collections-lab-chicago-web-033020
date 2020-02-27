37c197b49713906e2597d19321305b0d8028128d
Shau Yin Luk <ivan.luk@icloud.com>
2020-02-27 20:32:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 def find_the_cheese(list)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  list.find {
+  list.find {|item| 
     if list.include?(cheese_types[i]) 
       return cheese_types[i]
     end
